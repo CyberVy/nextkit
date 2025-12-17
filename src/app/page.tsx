00aee8163fa2c03495e3b02748cfb462f0dad9b7
CyberVy <923353275@qq.com>
2025-12-17 16:53:42 +0800

    chore: register service worker automatically

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
 "use client"
-
-
+import { useEffect } from "react"
 
 export default function Page(){
+
+    // register service worker
+    useEffect(() => {
+        if ('serviceWorker' in navigator) {
+            navigator.serviceWorker.register('/sw.js').then(() => {
+                console.log('Service worker is registered successfully.')
+            }).catch(err => {console.error('Failed to register Service worker.', err)})}
+    }, [])
+
     return (
         <div className="text-center">
             Hello World!
