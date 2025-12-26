b5a7e36f10e9aee3d293e3da6ccc09393e565947
CyberVy <923353275@qq.com>
2025-12-26 17:57:37 +0800

    ui: improve launch animation

--- src/components/LaunchAnimation.tsx
+++ src/components/LaunchAnimation.tsx
@@ -17,14 +17,17 @@ function LaunchAnimation() {
     }, [])
     return (
         <div
-            className={"relative h-[calc(100vh-env(safe-area-inset-top))] w-[100vw] overflow-hidden"}
+            className={"relative h-[calc(100vh-env(safe-area-inset-top))] w-[100vw] overflow-hidden select-none"}
         >
-            <div className={`text-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2`}>
-                <AnimatedGlowText
-                    text={welcome_sentence}
-                    stagger={0.06}
-                />
-            </div>
+            {is_amount &&
+                <div className={`text-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2`}>
+                    <AnimatedGlowText
+                        text={welcome_sentence}
+                        stagger={0.06}
+                    />
+                </div>
+            }
+
             <div className={`${is_amount ? "translate-x-[100vw]" : "-translate-x-1/1"} left-0 top-3/7 -translate-y-3/7 absolute transition duration-1500`}>
                 <div className={"w-12 h-12"}>
                     <LabeledImage
