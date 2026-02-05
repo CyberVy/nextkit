763ac5b7dac2ddc0e7a6f901bec992f8c81ccc40
CyberVy <923353275@qq.com>
2026-02-06 01:01:52 +0800

    improve(ios-native): remove WKWebkit flicker

---
+++ src-tauri/gen/apple/Sources/app/no_flicker.swift
@@ -0,0 +1,17 @@
+import UIKit
+import WebKit
+
+@_cdecl("on_webview_created")
+public func on_webview_created(_ webviewPtr: UnsafeRawPointer?, _ controllerPtr: UnsafeRawPointer?) {
+    guard let webviewPtr, let controllerPtr else { return }
+
+    let webview = Unmanaged<WKWebView>.fromOpaque(webviewPtr).takeUnretainedValue()
+    let controller = Unmanaged<UIViewController>.fromOpaque(controllerPtr).takeUnretainedValue()
+
+    let bg = UIColor.systemBackground
+
+    controller.view.backgroundColor = bg
+    webview.isOpaque = false
+    webview.backgroundColor = bg
+    webview.scrollView.backgroundColor = bg
+}
