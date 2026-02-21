6936d0ec032aa9abc2db6a050d1a6aa3bedf1393
CyberVy <923353275@qq.com>
2026-02-21 23:42:29 +0800

    improve(native): support js injection in webviews(frames)

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,8 @@
 mod fetch;
 mod no_flicker;
 
+const INJECT_SCRIPT: &str = include_str!("./inject.js");
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
@@ -12,6 +14,25 @@ pub fn run() {
                         .build(),
                 )?;
             }
+
+            let main_window_config = app
+                .config()
+                .app
+                .windows
+                .iter()
+                .find(|window_config| window_config.label == "main")
+                .or_else(|| app.config().app.windows.first())
+                .expect("main window config not found");
+
+            let inject_script = INJECT_SCRIPT.trim();
+            let mut webview_builder =
+                tauri::WebviewWindowBuilder::from_config(app, main_window_config)?;
+            if !inject_script.is_empty() {
+                webview_builder =
+                    webview_builder.initialization_script_for_all_frames(inject_script);
+            }
+            webview_builder.build()?;
+
             Ok(())
         })
         .invoke_handler(tauri::generate_handler![
