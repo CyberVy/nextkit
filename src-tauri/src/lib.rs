b0f0133e9293e1870c3648534c61dca17aa398b7
CyberVy <923353275@qq.com>
2026-02-28 23:30:05 +0800

    improve(desktop-native): window size memorization

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -6,6 +6,7 @@ const INJECT_SCRIPT: &str = include_str!("./inject.js");
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_window_state::Builder::new().build())
         .setup(|app| {
             if cfg!(debug_assertions) {
                 app.handle().plugin(
